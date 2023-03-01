5d5c7b6463b98c7378e335b1f3f82c09f6236e2f
Damy Metzke <damy.metzke@gmail.com>
2023-03-01 22:10:22 +0100

    Write CLI interface for `ls [CATEGORY]`

--- src/cli/main.rs
+++ src/cli/main.rs
@@ -1,13 +1,43 @@
 use clap::{Parser, Subcommand};
-use grass::list_categories;
+use grass::{list_categories, list_repos_by_category};
 use itertools::Itertools;
+
+#[derive(Parser, Debug)]
+struct LsCommand {
+    category: Option<String>,
+}
+
+fn handle_ls(command: LsCommand) {
+    let category = if let Some(result) = command.category {
+        result
+    } else {
+        println!(
+            "Categories:\n\n{}",
+            list_categories()
+                .iter()
+                .map(|category| format!("* {}", category))
+                .join("\n")
+        );
+        return;
+    };
+
+    let category = list_repos_by_category(category);
+    println!(
+        "Repos for category '{}':\n\n{}",
+        category.category,
+        category
+            .repositories
+            .iter()
+            .map(|category| format!("* {}", category))
+            .join("\n")
+    );
+}
 
 #[derive(Debug, Subcommand)]
 enum Command {
-    Ls,
+    Ls(LsCommand),
 }
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
@@ -19,12 +49,6 @@ fn main() {
     let args = Args::parse();
 
     match args.command {
-        Command::Ls => println!(
-            "Categories:\n\n{}",
-            list_categories()
-                .iter()
-                .map(|category| format!("* {}", category))
-                .join("\n")
-        ),
+        Command::Ls(command) => handle_ls(command),
     }
 }
--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,6 +1,6 @@
 /// List categories
 ///
-/// # Example:
+/// # Examples
 ///
 /// ```
 /// # use grass::list_categories;
@@ -8,5 +8,31 @@
 /// assert_eq!(vec!{"general", "work"}, list_categories());
 /// ```
 pub fn list_categories() -> Vec<String> {
-    vec!{String::from("general"), String::from("work")}
+    vec![String::from("general"), String::from("work")]
+}
+
+pub struct SimpleCategoryDescription {
+    pub category: String,
+    pub repositories: Vec<String>,
+}
+
+/// List repositories in a single category
+///
+/// # Examples
+///
+/// ```
+/// # use grass::list_repos_by_category;
+///
+/// let result_general = list_repos_by_category("general");
+/// let result_work = list_repos_by_category("work");
+///
+/// assert_eq!(vec!["first", "second"], result_general.repositories);
+/// assert_eq!("general", result_general.category);
+/// assert_eq!("work", result_work.category);
+/// ```
+pub fn list_repos_by_category<T: AsRef<str>>(category_name: T) -> SimpleCategoryDescription {
+    SimpleCategoryDescription {
+        category: String::from(category_name.as_ref()),
+        repositories: vec![String::from("first"), String::from("second")],
+    }
 }
