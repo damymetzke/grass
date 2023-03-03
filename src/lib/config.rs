f27667e0a4e89b650c6baea027d90d38a3440327
Damy Metzke <damy.metzke@gmail.com>
2023-03-03 20:49:34 +0100

    Add support for multiple configuration files
    Load every file in the `.config/grass` directory,
    then merge the configurations.

--- src/lib/config.rs
+++ src/lib/config.rs
@@ -1,6 +1,10 @@
 mod load;
 
-use std::{collections::hash_map::HashMap, fs::File, io::Read};
+use std::{
+    collections::hash_map::HashMap,
+    fs::{self, File},
+    io::Read,
+};
 
 use self::load::{LoadGrassCategory, LoadRootConfig};
 
@@ -54,6 +58,31 @@ pub fn load_user_config() -> Result<RootConfig, Box<dyn std::error::Error>> {
     let mut config = RootConfig::default();
     let load_config: LoadRootConfig = toml::from_str(&contents)?;
     config.merge(load_config);
+
+    let mut config = RootConfig::default();
+
+    let config_dir = dirs::config_dir()
+        .ok_or("Could not find config directory")?
+        .join("grass");
+    if let Ok(entries) = fs::read_dir(&config_dir) {
+        for file_name in
+            entries
+                .filter_map(|entry| entry.ok())
+                .filter_map(|entry| match entry.metadata() {
+                    Ok(metadata) if metadata.is_file() => Some(entry.file_name()),
+                    _ => None,
+                })
+        {
+            // TODO: better handle these errors, this should not stop the program.
+            let mut file = File::open(&config_dir.join(file_name))?;
+
+            let mut contents = String::new();
+            file.read_to_string(&mut contents)?;
+            let load_config: LoadRootConfig = toml::from_str(&contents)?;
+            config.merge(load_config);
+        }
+    }
+
     Ok(config)
 }
 
