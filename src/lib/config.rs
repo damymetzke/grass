d03d9eb1f1a6cb099d0ae7802819718eb443c939
Damy Metzke <damy.metzke@gmail.com>
2023-03-03 20:16:44 +0100

    Update how user configuration loads
    Load configuration using a different set of structs.
    These structs will allow optional values.
    This is used in case multiple files need to be merged.

--- src/lib/config.rs
+++ src/lib/config.rs
@@ -1,19 +1,45 @@
+mod load;
+
 use std::{collections::hash_map::HashMap, fs::File, io::Read};
 
-use serde::Deserialize;
+use self::load::{LoadGrassCategory, LoadRootConfig};
 
-#[derive(Deserialize, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Default)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub struct GrassCategory {}
 
-#[derive(Deserialize, Debug, Clone, PartialEq, Eq, Default)]
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
 pub struct GrassConfig {
     pub category: HashMap<String, GrassCategory>,
 }
 
-#[derive(Deserialize, Debug, Clone, PartialEq, Eq, Default)]
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
 pub struct RootConfig {
-    #[serde(default)]
     pub grass: GrassConfig,
+}
+
+impl From<LoadGrassCategory> for GrassCategory {
+    fn from(_value: LoadGrassCategory) -> GrassCategory {
+        GrassCategory {}
+    }
+}
+
+impl RootConfig {
+    pub fn merge(&mut self, next: LoadRootConfig) -> &mut Self {
+        let grass = if let Some(grass) = next.grass {
+            grass
+        } else {
+            return self;
+        };
+
+        for (key, grass_category) in grass.category {
+            if let std::collections::hash_map::Entry::Vacant(e) = self.grass.category.entry(key) {
+                e.insert(grass_category.into());
+            } else {
+            };
+        }
+
+        self
+    }
 }
 
 pub fn load_user_config() -> Result<RootConfig, Box<dyn std::error::Error>> {
@@ -25,7 +51,9 @@ pub fn load_user_config() -> Result<RootConfig, Box<dyn std::error::Error>> {
 
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
-    let config: RootConfig = toml::from_str(&contents)?;
+    let mut config = RootConfig::default();
+    let load_config: LoadRootConfig = toml::from_str(&contents)?;
+    config.merge(load_config);
     Ok(config)
 }
 
---
+++ src/lib/config/load.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Default)]
+pub struct LoadGrassCategory {}
+
+#[derive(Deserialize, Debug, Clone, PartialEq, Eq, Default)]
+pub struct LoadGrassConfig {
+    pub category: HashMap<String, LoadGrassCategory>,
+}
+
+#[derive(Deserialize, Debug, Clone, PartialEq, Eq, Default)]
+pub struct LoadRootConfig {
+    pub grass: Option<LoadGrassConfig>,
+}
