c614e1c1f16804f75c511f0c79d917daddd6b4c5
Damy Metzke <damy.metzke@gmail.com>
2023-03-09 20:12:21 +0100

    Implement bash insertion

--- src/cli/grass_command/shell_insert.rs
+++ src/cli/grass_command/shell_insert.rs
@@ -1,4 +1,7 @@
+use std::{env, process::Command as ProcessCommand, str};
+
 use clap::{Parser, ValueEnum};
+use grass::config;
 
 #[derive(Debug, Clone, Copy, ValueEnum)]
 pub enum Shells {
@@ -7,11 +10,34 @@ pub enum Shells {
 
 #[derive(Parser, Debug)]
 pub struct ShellInsertCommand {
-    shell: Shells
+    shell: Shells,
 }
 
 impl ShellInsertCommand {
     pub fn handle(&self) {
-        //TODO: Handle shells
+        match self.shell {
+            Shells::Bash => Self::handle_bash(),
+        };
+    }
+
+    fn handle_bash() {
+        let user_config = config::load_user_config().unwrap_or_default();
+        // Check for TMUX variable
+        if env::var("TMUX").is_ok() {
+            if let Ok(Ok(output)) = ProcessCommand::new("tmux")
+                .args(["display-message", "-p", "#S"])
+                .output()
+                .map(|output| str::from_utf8(&output.stdout).map(String::from))
+            {
+                if let [repository, category] = output.trim().split('@').collect::<Vec<_>>().as_slice() {
+                    if let Some(Some(path)) =
+                        grass::get_repository_path(&user_config, category, repository)
+                            .map(|path| path.to_str().map(String::from))
+                    {
+                        println!("cd {}", path);
+                    };
+                };
+            };
+        };
     }
 }
