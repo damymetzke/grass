9c6bb3e7a94c2887b0a31454dd2bdde8e12b0999
Damy Metzke <damy.metzke@gmail.com>
2023-05-24 16:55:57 +0200

    Add simple format to `check changes`

--- src/cli/grass_command/check/changes.rs
+++ src/cli/grass_command/check/changes.rs
@@ -1,41 +1,62 @@
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use grass::{
     config,
     repository::RepositoryChangeStatus,
     types::{FilteredCategoryDescription, SimpleRepositoryDescription},
 };
+use itertools::Itertools;
 
 use crate::output::generate_fancy_vertical_list;
 use colored::*;
+
+#[derive(ValueEnum, Debug, Clone, Default)]
+enum Format {
+    #[default]
+    Fancy,
+    Simple,
+}
 
 #[derive(Parser, Debug)]
 pub struct ChangesCommand {
     category: Option<String>,
     #[clap(short, long)]
     all: bool,
+    #[clap(long)]
+    format: Option<Format>,
 }
 
 impl ChangesCommand {
-    fn handle_category<T>(category: FilteredCategoryDescription<T, RepositoryChangeStatus>)
-    where
+    fn handle_category<T>(
+        category: FilteredCategoryDescription<T, RepositoryChangeStatus>,
+        format: &Format,
+    ) where
         T: Iterator<Item = (SimpleRepositoryDescription, RepositoryChangeStatus)>,
     {
-        println!(
-            "{}",
-            generate_fancy_vertical_list(
-                format!(
-                    "Repositories with uncommitted changes for '{}'",
-                    category.category
-                ),
+        match format {
+            Format::Fancy => println!(
+                "{}",
+                generate_fancy_vertical_list(
+                    format!(
+                        "Repositories with uncommitted changes for '{}'",
+                        category.category
+                    ),
+                    category
+                        .repository_iterator
+                        .map(|(repository, status)| format!(
+                            "{} ({})",
+                            repository.repository.bright_blue(),
+                            status.to_string().red()
+                        ))
+                )
+            ),
+            Format::Simple => println!(
+                "{}",
                 category
                     .repository_iterator
-                    .map(|(repository, status)| format!(
-                        "{} ({})",
-                        repository.repository.bright_blue(),
-                        status.to_string().red()
-                    ))
-            )
-        )
+                    .map(|(repository, _)| repository.repository)
+                    .join(" ")
+            ),
+        }
     }
 
     pub fn handle(&self) {
@@ -45,6 +66,7 @@ impl ChangesCommand {
             Self {
                 category: Some(category),
                 all: false,
+                format,
             } => {
                 let category = grass::list_repositories_with_change_status(&user_config, category);
                 let category = if let Some(repositories) = category {
@@ -54,25 +76,28 @@ impl ChangesCommand {
                     eprintln!("Category or alias not found");
                     return;
                 };
-                Self::handle_category(category);
+                Self::handle_category(category, &format.clone().unwrap_or_default());
             }
             Self {
                 category: None,
                 all: true,
+                format,
             } => {
                 for category in grass::list_all_repositories_with_change_status(&user_config) {
                     let category = category
                         .filter(|(_, status)| !matches!(status, RepositoryChangeStatus::UpToDate));
-                    Self::handle_category(category);
+                    Self::handle_category(category, &format.clone().unwrap_or_default());
                 }
             }
             Self {
                 category: Some(_),
                 all: true,
+                format: _,
             } => eprintln!("Received incompatible category with all flag"),
             Self {
                 category: None,
                 all: false,
+                format: _,
             } => eprintln!("Provide either a category, or the --all flag"),
         }
     }
